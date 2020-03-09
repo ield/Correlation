a9b11a8cfa2aa6ceb1dee04bc6d6dea3faaccfb2
ield <nachol2013@gmail.com>
2020-03-09 10:58:59 +0100

    signalSNR calculates the snr of the function

--- signalSNR.m
+++ signalSNR.m
@@ -13,6 +13,22 @@ function [snr] = signalSNR(signal1, signal2, correlation)
 powS1 = sigPower(signal1);
 powS2 = sigPower(signal2);
 
+%% 2. Calculates k
+% k stands for the attenuation caused to the signal
+
+corMax = max(correlation);
+k = corMax / (powS1 * length(signal1));
+
+%% 3. Calculates s2 and n2
+% s2 stands for only the signal component in signal2
+% n2 stands for only the noise component in signal2
+
+s2 = powS1 * (k^2);
+n2 = powS2 - s2;
+
+%% 4. Calculates snr in signal2
+snr = 10*log10(s2/n2);
+
 snr = 0;
 
 end
