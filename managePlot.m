ee24fea6df37e8a9787491c9ff95297bdb22be3f
ield <nachol2013@gmail.com>
2020-04-14 19:21:26 +0200

    Changing background color and font

--- managePlot.m
+++ managePlot.m
@@ -12,8 +12,11 @@ function [] = managePlot(xaxis, fourier)
 % xaxis:    x axis of the plot. It is used to limit the x axis
 % fourier:  true if the graph is the one of a fourier correlation
 
-    xlabel('Distance [m]');
-    ylabel('Correlation');
+    xlabel('Distance [m]', 'FontName','Times New Roman','FontSize', 9);
+    ylabel('Correlation', 'FontName','Times New Roman','FontSize', 9);
+    
+    tick = get(gca,'TickLabel');
+    set(gca,'TickLabel',tick,'FontName','Times New Roman','fontsize',9)
     
     if(fourier)
         title('Fourier Correlation');
@@ -24,6 +27,7 @@ function [] = managePlot(xaxis, fourier)
     xlim([xaxis(1) xaxis(length(xaxis))]);
     
     set(gca, 'ytick', []);
+    
     
 end
 
